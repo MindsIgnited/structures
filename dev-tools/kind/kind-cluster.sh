c5c12802be20ccad1d8722b7788d4f9f5c50e574
Nicholas Padilla <nicholas@mindsignited.com>
2026-06-09 18:24:45 -0700

    fix: forgot to bump the version

--- dev-tools/kind/kind-cluster.sh
+++ dev-tools/kind/kind-cluster.sh
@@ -728,7 +728,7 @@ Examples:
   $(basename "$0") load
 
   # Load specific image
-  $(basename "$0") load --image mindsignited/structures-server:3.5.4
+  $(basename "$0") load --image mindsignited/structures-server:3.5.6
 
   # Load into specific cluster
   $(basename "$0") load --name test-cluster
